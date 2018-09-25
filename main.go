26ce8043da6cb9cdf17924cf9216fb5f1e39f78d
Rahul <rgudimetla@researchnow.com>
2018-09-25 18:12:38 -0500

    call deck array and print them

--- main.go
+++ main.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-	"fmt"
-)
-
 func main() {
-	// var card string = "Hello world"
+	cards := newDeck()
 
-	card := newCard() // both are same, assigning data to a variable
-	// card = "Hello guru"   // re-assign variable declaration
-
-	fmt.Println(card)
-
-	cards := deck{newCard(), "new array of cards"}
-
-	deck.print(cards)
-
-}
-
-func newCard() string {
-	return "returned string"
+	cards.print()
 }
